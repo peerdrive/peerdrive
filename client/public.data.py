2a84b42757d5c428aa3a6f403224f2a69332ac8f
Jan Klötzke <jan.kloetzke@freenet.de>
2010-12-21 22:04:05 +0100

    Convert public.data.py to client/server

--- client/public.data.py
+++ client/public.data.py
@@ -17,59 +17,167 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from PyQt4 import QtCore, QtNetwork
 import sys, tempfile, os.path, subprocess
 
 from hotchpotch import struct, Connector, Registry
 
+PIPE_NAME = "org.hotchpotch.oslauncher"
 
-# parse command line
-if len(sys.argv) == 2 and sys.argv[1].startswith('doc:'):
-	doc = sys.argv[1][4:].decode("hex")
-	rev = Connector().lookup_doc(doc).revs()[0]
-elif len(sys.argv) == 2 and sys.argv[1].startswith('rev:'):
-	rev = sys.argv[1][4:].decode("hex")
-else:
-	print "Usage: public.data.py <Document> ..."
+
+class RequestManager(QtCore.QObject):
+
+	finished = QtCore.pyqtSignal()
+
+	def __init__(self, parent=None):
+		super(RequestManager, self).__init__(parent)
+		self.__server = None
+		self.__clients = []
+		self.__quitRequested = False
+
+	def request(self, request):
+		self.__request = request
+		self.__socket = QtNetwork.QLocalSocket(self)
+		self.__socket.connected.connect(self.__clientConnect)
+		self.__socket.error.connect(self.__clientError)
+		self.__socket.connectToServer(PIPE_NAME)
+		return not self.__quitRequested
+
+	def serve(self):
+		self.__server = QtNetwork.QLocalServer(self)
+		self.__server.newConnection.connect(self.__serverConnect)
+		if not self.__server.listen(PIPE_NAME):
+			print "Unable to listen: ", self.__server.errorString()
+			self.__finished()
+		return not self.__quitRequested
+
+	def __finished(self):
+		self.__quitRequested = True
+		if self.__server:
+			self.__server.close()
+		for client in self.__clients:
+			client.close()
+		self.finished.emit()
+
+	def __clientConnect(self):
+		self.__socket.write(self.__request + '\n')
+		self.__socket.disconnected.connect(self.__finished)
+		self.__socket.disconnectFromServer()
+
+	def __clientError(self, error):
+		if self.__request == "kill":
+			print "Could not kill server:", error
+			self.__finished()
+		elif error == QtNetwork.QLocalSocket.ServerNotFoundError:
+			# dispose client socket
+			self.__socket.setParent(None)
+			del self.__socket
+
+			# start server socket
+			self.serve()
+
+			# process local request in any case
+			self.__process(self.__request)
+		else:
+			print "Unexpected QLocalSocket error:", error
+			self.__finished()
+
+	def __serverConnect(self):
+		socket = self.__server.nextPendingConnection()
+		if socket:
+			self.__clients.append(socket)
+			socket.disconnected.connect(lambda s=socket: self.__serverDisconnect(s))
+			socket.readyRead.connect(lambda s=socket: self.__serverReady(s))
+			self.__serverReady(socket)
+
+	def __serverDisconnect(self, socket):
+		self.__clients.remove(socket)
+		socket.setParent(None)
+		socket.deleteLater()
+
+	def __serverReady(self, socket):
+		if socket.canReadLine():
+			self.__process(str(socket.readLine())[:-1])
+
+	def __process(self, request):
+		if request.startswith('doc:'):
+			doc = request[4:].decode("hex")
+			rev = Connector().lookup_doc(doc).revs()[0]
+		elif request.startswith('rev:'):
+			rev = request[4:].decode("hex")
+		elif request == "kill":
+			self.__finished()
+			return
+		else:
+			return
+
+		stat = Connector().stat(rev)
+		uti  = stat.type()
+		hash = stat.hash('FILE')
+
+		# determine extension
+		extensions = Registry().search(uti, "extensions")
+		if extensions:
+			ext = extensions[0]
+		else:
+			ext = ""
+			with Connector().peek(rev) as r:
+				meta = struct.loads(r.readAll('META'))
+
+			if "org.hotchpotch.annotation" in meta:
+				annotation = meta["org.hotchpotch.annotation"]
+				# try origin
+				if "origin" in annotation:
+					ext  = os.path.splitext(annotation["origin"])[1]
+				# try title
+				if not ext:
+					if "title" in annotation:
+						ext = os.path.splitext(annotation["title"])[1]
+
+		# copy out file (if necessary)
+		path = os.path.join(tempfile.gettempdir(), hash.encode('hex')+ext)
+		if not os.path.isfile(path):
+			with open(path, "wb") as file:
+				with Connector().peek(rev) as reader:
+					file.write(reader.readAll('FILE'))
+
+		# start external program
+		if sys.platform == "win32":
+			subprocess.call(["start", path], shell=True)
+		else:
+			subprocess.call(["xdg-open", path])
+
+
+def usage():
+	print "Usage: public.data.py [Request]"
 	print
-	print "Document:"
+	print "Request:"
 	print "    doc:<document>  ...open the latest version of the given document"
 	print "    rev:<revision>  ...display the given revision"
+	print "    kill            ...kill server"
+	print
+	print "If no request is given then a server is started."
 	sys.exit(1)
 
 
-stat = Connector().stat(rev)
-uti  = stat.type()
-hash = stat.hash('FILE')
+if len(sys.argv) > 2:
+	usage()
+elif (len(sys.argv) == 2 and not
+		(sys.argv[1].startswith('doc:') or
+		sys.argv[1].startswith('rev:') or
+		sys.argv[1] == "kill")):
+	usage()
 
-# determine extension
-extensions = Registry().search(uti, "extensions")
-if extensions:
-	ext = extensions[0]
+app = QtCore.QCoreApplication(sys.argv)
+mgr = RequestManager(app)
+mgr.finished.connect(app.quit)
+if len(sys.argv) > 1:
+	start = mgr.request(sys.argv[1])
 else:
-	ext = ""
-	with Connector().peek(rev) as r:
-		meta = struct.loads(r.readAll('META'))
+	start = mgr.serve()
 
-	if "org.hotchpotch.annotation" in meta:
-		annotation = meta["org.hotchpotch.annotation"]
-		# try origin
-		if "origin" in annotation:
-			ext  = os.path.splitext(annotation["origin"])[1]
-		# try title
-		if not ext:
-			if "title" in annotation:
-				ext = os.path.splitext(annotation["title"])[1]
-
-# copy out file (if necessary)
-path = os.path.join(tempfile.gettempdir(), hash.encode('hex')+ext)
-if not os.path.isfile(path):
-	with open(path, "wb") as file:
-		with Connector().peek(rev) as reader:
-			file.write(reader.readAll('FILE'))
-
-# start external program
-if sys.platform == "win32":
-	subprocess.Popen(["start", path], shell=True)
+if start:
+	sys.exit(app.exec_())
 else:
-	subprocess.Popen(["xdg-open", path])
+	sys.exit(1)
 
